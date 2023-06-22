5744006818a2ac70e393ec4e608d6c25d0f3e58d
takahashi-pao <a.mapi.baseball@gmail.com>
2023-06-22 20:59:58 +0900

    add

--- opencv.py
+++ opencv.py
@@ -1,10 +1,17 @@
 import cv2
 
-image1 = cv2.imread('ルッチ1.png')
-image2 = cv2.imread('ルッチ2.png')
+# ファイル名は英数字（半角）
+# xxx.pyと同じ階層に置く（ここで言うとopencv.pyと同じ階層）
+image1 = cv2.imread('1.png')
+image2 = cv2.imread('2.png')
 
+# 差分を取得
 result = cv2.subtract(image1, image2)
 
+# resultというウィンドウ名で差分を表示
 cv2.imshow('result', result)
+
+# 何かしらのキーが押下されるまで待機
 cv2.waitKey(0)
+# キー押下でウィンドウを破棄
 cv2.destroyAllWindows()
